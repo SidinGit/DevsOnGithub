e39b77331d128bfa6da6d57db57484d07f0db960
Siddhartha <sidskole@gmail.com>
2024-12-30 12:38:39 +0530

    integrate github api to frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import { Navigate, Route, Routes } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
+import LoginPage from './pages/LoginPage'
+import HomePage from './pages/HomePage'
+import SignUpPage from './pages/SignUpPage'
+import ExplorePage from './pages/ExplorePage'
+import LikesPage from './pages/LikesPage'
+import Sidebar from './components/Sidebar'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+  const authUser = true
+  return (
+    <div className='flex text-white'>
+			<Sidebar />
+			<div className='max-w-5xl my-5 text-white mx-auto transition-all duration-300 flex-1'>
+				<Routes>
+					<Route path='/' element={<HomePage />} />
+					<Route path='/login' element={!authUser ? <LoginPage /> : <Navigate to={"/"} />} />
+					<Route path='/signup' element={!authUser ? <SignUpPage /> : <Navigate to={"/"} />} />
+					<Route path='/explore' element={authUser ? <ExplorePage /> : <Navigate to={"/login"} />} />
+					<Route path='/likes' element={authUser ? <LikesPage /> : <Navigate to={"/login"} />} />
+				</Routes>
+				<Toaster />
+			</div>
+		</div>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,85 @@
+import React, { useCallback, useEffect, useState } from 'react'
+import Search from '../components/Search';
+import SortRepos from '../components/SortRepos';
+import ProfileInfo from '../components/ProfileInfo';
+import Repos from '../components/Repos';
+import Spinner from '../components/Spinner';
+import toast from 'react-hot-toast';
+
+const HomePage = () => {
+	const [userProfile, setUserProfile] = useState(null)
+	const [repos, setRepos] = useState([])
+	const [loading, setLoading] = useState(false)
+
+	const [sortType, setSortType] = useState("recent")
+	const user = true
+	const getUserProfileAndRepos = useCallback(async ( username="SidinGit" ) => {
+		setLoading(true)
+		try {
+			const userRes = await fetch(`https://api.github.com/users/${username}`,
+				{
+					headers: {
+						authorization: `token ${import.meta.env.VITE_GITHUB_API_KEY}`
+					}
+				}
+			)
+			const userProfile = await userRes.json()
+			setUserProfile(userProfile)
+			// console.log(userProfile)
+			const repoRes = await fetch(userProfile.repos_url)
+			const repos = await repoRes.json()
+			repos.sort((a,b) => new Date(b.created_at) - new Date(a.created_at))
+			setRepos(repos)
+			// console.log(repos)
+			return {userProfile, repos}
+		} catch (error) {
+			toast.error(error.message)
+		} finally {
+			setLoading(false)
+		}
+	}, [])
+
+	useEffect(() => {
+		getUserProfileAndRepos()
+	},[getUserProfileAndRepos])
+	
+	const onSearch = async(e,username) => {
+		e.preventDefault()
+
+		setLoading(true)
+		setRepos([])
+		setUserProfile(null)
+
+		const {userProfile, repos} = await getUserProfileAndRepos(username)
+		setRepos(repos)
+		setUserProfile(userProfile)
+		setLoading(false)
+		setSortType('recent')
+	}
+
+	const onSort = (sortType) => {
+		if(sortType === "recent") {
+			repos.sort((a,b) => new Date(b.created_at) - new Date(a.created_at)) // recent first
+		} else if(sortType === "stars") {
+			repos.sort((a,b) => b.stargazers_count - a.stargazers_count) // most stars first
+		} else if(sortType === "forks") {
+			repos.sort((a,b) => b.forks_count - a.forks_count) // most forks first
+		}
+		setSortType(sortType)
+		setRepos([...repos])
+	}
+
+	return (
+		<div className='m-4'>
+			<Search onSearch = {onSearch} />
+			{repos.length > 0 && <SortRepos onSort={onSort} sortType={sortType}/>}
+			<div className='flex gap-4 flex-col lg:flex-row justify-center items-start'>
+				{userProfile && !loading &&<ProfileInfo userProfile={userProfile}/>}
+				{!loading &&<Repos repos={repos}/>}
+				{loading && <Spinner />}
+			</div>
+		</div>
+	);
+};
+
+export default HomePage
