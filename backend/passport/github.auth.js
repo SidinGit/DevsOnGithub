404a7848be5aec89c9d5047a87b8d53b845866ef
Siddhartha <sidskole@gmail.com>
2025-01-07 06:18:43 +0530

    update callback url

--- backend/passport/github.auth.js
+++ backend/passport/github.auth.js
@@ -22,7 +22,7 @@ passport.use(
     new GitHubStrategy({
         clientID: process.env.GITHUB_CLIENT_ID,
         clientSecret: process.env.GITHUB_CLIENT_SECRET,
-        callbackURL: "/api/auth/github/callback"
+        callbackURL: "https://devs-on-github.onrender.com/api/auth/github/callback"
     },
     async function(accessToken, refreshToken, profile, done) {
     // asynchronous verification, for effect...
