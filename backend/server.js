a40870f579ed3f4fd628b0c7e59b16d24e596cf8
Siddhartha <sidskole@gmail.com>
2025-01-07 06:04:20 +0530

    optimize for build

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-
 import passport from "passport";
 import session from "express-session";
+import path from "path";
+
 import "./passport/github.auth.js";
+
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
 import exploreRoutes from "./routes/explore.route.js";
@@ -12,6 +14,10 @@ import connectMongoDB from "./db/connectMongoDB.js";
 
 dotenv.config();
 const app = express();
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
+
+console.log("dirname", __dirname);
 
 app.use(
   session({ secret: "keyboard cat", resave: false, saveUninitialized: false })
@@ -23,15 +29,16 @@ app.use(passport.session());
 
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("server is running");
-});
-
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/explore", exploreRoutes);
 
-app.listen(5000, (req, res) => {
-  console.log("server is running at http://localhost:5000");
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
+app.listen(PORT, (req, res) => {
+  console.log(`server is running at http://localhost:${PORT}`);
   connectMongoDB();
 });
